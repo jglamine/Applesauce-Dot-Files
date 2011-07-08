b4fbbcfd7eeffeda043c1ee11e52790fcaf762b4
James Lamine <jglamine2@gmail.com>
2011-07-07 22:38:56 -0400

    Commit

--- git-backup.py
+++ git-backup.py
@@ -2,7 +2,7 @@
 # Script to copy dot files to a local git repository and push that repo to git-hub
 # The list of files to backup is saved in a text file
 
-import sys, shutil, subprocess, re
+import sys, shutil, subprocess, re, os
 
 # Configuration variables
 listFname = "backup-filelist"
@@ -15,9 +15,32 @@ except IOError:
 	print ("Error: Unable to open input file:", listFname)
 	sys.exit(1)
 
-# Copy files to the git repo directory
+# Copy files to the git repo directory, only if they have changed
 for path in listFile:
-	shutil.copy(path.strip(), gitRepoPath)
+	path = path.strip()
+	
+	# initialize variables
+	sourceMtime = 900000000000
+	destMtime = 0
+	try:
+		# os.stat will throw OSError if the file doesn't exits
+		sourceMtime = os.stat("path").st_mtime
+		
+		# find the name of the destination file
+		fnameMatch = re.search(r"[^/]*$", path)
+		if fnameMatch:
+			destPath = path + fnameMatch.group()
+			destMtime = os.stat(destPath).st_mtime
+		
+	except OSError:
+		pass
+	
+	# only copy files if the source is newer than the destination file
+	if destMtime < sourceMtime:
+		shutil.copy(path, gitRepoPath)
+	else:
+		print ("No changes to copy")
+		sys.exit(0)
 
 # Close the filelist file
 listFile.close()
